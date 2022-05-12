f4db248fd368c1bab348ca99f27236a9ca53238b
szuzanna <zdrapka111@gmail.com>
2022-05-12 23:51:49 +0200

    task2

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ app = FastAPI()
 @app.get("/",status_code=200)
 def root():
     return {"start": "1970-01-01"}
+
+@app.get("/method",status_code=200)
+def root():
+    return {"method": "GET"}
+
+@app.put("/method",status_code=200)
+def root():
+    return {"method": "PUT"}
+
+@app.options("/method",status_code=200)
+def root():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method",status_code=200)
+def root():
+    return {"method": "DELETE"}
+
+@app.post("/method",status_code=201)
+def root():
+    return {"method": "POST"}

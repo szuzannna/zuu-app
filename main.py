606646059ea6690c70f9e65173b79cea60c418da
szuzanna <zdrapka111@gmail.com>
2022-05-15 22:55:07 +0200

    task 3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi import status
+from fastapi import Response
+import asyncio
 
 app = FastAPI()
 
@@ -25,3 +28,18 @@ def root():
 @app.post("/method",status_code=201)
 def root():
     return {"method": "POST"}
+
+@app.get("/day", status_code=200)
+def read_item(name: str, number: int, response: Response):
+    day_dictionary= {"monday" : 1,
+                     "tuesday" : 2,
+                     "wednesday" : 3,
+                     "thursday" :4,
+                     "friday" : 5,
+                     "saturday" :6,
+                     "sunday" :7}
+    if name.lower() not in day_dictionary:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+    elif day_dictionary[name.lower()] != number:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+    return response.status_code
